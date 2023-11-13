1b6668f03faf58003282c9e1cb2b1000a3b81421
Facundo Lescano <flescano@fi.uba.ar>
2023-11-12 22:39:15 -0300

    implementacion y pruebas

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -1,17 +1,259 @@
 #include "pa2m.h"
+#include "src/hash.h"
+#include <stdlib.h>
+#include <string.h>
 
-void prueba_simple()
+const char *clave1 = "A";
+const char *clave2 = "B";
+const char *clave3 = "C";
+const char *clave4 = "AB";
+const char *clave5 = "BB";
+const char *clave6 = "CB";
+const char *clave7 = "CC";
+
+bool funcion_iteradora(const char *clave, void *valor, void *aux)
 {
-	int i = 14;
-	int j = 16;
-	pa2m_afirmar(i != j, "i=14 es diferente de j=16");
+	if (valor == aux) {
+		return false;
+	}
+	return true;
+}
+
+void destructor(void *elemento)
+{
+	return;
+}
+
+void prueba_creacion()
+{
+	hash_t *hash1 = hash_crear(0);
+	pa2m_afirmar(hash1 != NULL,
+		     "Se crea hash con capacidad menor a 3 y destruyo");
+	hash_destruir(hash1);
+	hash_t *hash2 = hash_crear(4);
+	pa2m_afirmar(hash2 != NULL,
+		     "Se crea hash con capacidad mayor a 3 y destruyo todo");
+	hash_destruir_todo(hash2, &destructor);
+	hash_t *hash3 = hash_crear(4);
+	pa2m_afirmar(
+		hash3 != NULL,
+		"Se crea hash con capacidad mayor a 3 y destruyo con destructor NULL");
+	hash_destruir_todo(hash3, NULL);
+}
+
+void prueba_hash_vacio()
+{
+	void *aux = NULL;
+	hash_t *hash = hash_crear(5);
+	pa2m_afirmar(hash_cantidad(hash) == 0, "Hash no tiene elementos");
+	pa2m_afirmar(hash_quitar(hash, clave1) == NULL,
+		     "No se puedo quitar en un hash vacio");
+	pa2m_afirmar(
+		hash_obtener(hash, clave1) == NULL,
+		"No se puede obtener el elemento de una clave en un hash vacio");
+	pa2m_afirmar(hash_contiene(hash, clave1) == false,
+		     "El hash no contiene elementos");
+	pa2m_afirmar(hash_con_cada_clave(hash, funcion_iteradora, aux) == 0,
+		     "No se puede iterar un hash vacio");
+	hash_destruir(hash);
+}
+
+void prueba_hash_nulo()
+{
+	void *aux = NULL;
+	hash_t *hash = NULL;
+	pa2m_afirmar(hash_cantidad(hash) == 0, "Hash nulo no tiene elementos");
+	pa2m_afirmar(hash_quitar(hash, clave1) == NULL,
+		     "No se puedo quitar en un hash nulo");
+	pa2m_afirmar(
+		hash_obtener(hash, clave1) == NULL,
+		"No se puede obtener el elemento de una clave en un hash nulo");
+	pa2m_afirmar(hash_contiene(hash, clave1) == false,
+		     "El hash no contiene elementos");
+	pa2m_afirmar(hash_con_cada_clave(hash, funcion_iteradora, aux) == 0,
+		     "No se puede iterar un hash nulo");
+	hash_destruir(hash);
+}
+
+void prueba_inserccion()
+{
+	int elemento1 = 1;
+	int elemento2 = 2;
+	int elemento3 = 3;
+	int elemento4 = 4;
+	int elemento5 = 5;
+	int elemento6 = 6;
+	void *elemento = &elemento1;
+	void **anterior = &elemento;
+
+	hash_t *hash = hash_crear(0);
+	pa2m_afirmar(hash_insertar(hash, NULL, &elemento3, anterior) == NULL,
+		     "No se puede insertar par con clave nula");
+	pa2m_afirmar(hash_insertar(hash, clave2, NULL, anterior) != NULL,
+		     "Se inserta un elemento NULL corectamente");
+	pa2m_afirmar(hash_insertar(hash, clave1, &elemento2, anterior) != NULL,
+		     "Se inserta un elemento con clave valida");
+	pa2m_afirmar(hash_insertar(hash, clave1, &elemento1, anterior) != NULL,
+		     "Se inserta un elemento con clave repetida");
+	pa2m_afirmar(*anterior == &elemento2,
+		     "El elemento anterior es el correcto");
+	pa2m_afirmar(hash_insertar(hash, clave2, &elemento2, anterior) != NULL,
+		     "Se inserta un elemento con clave repetida");
+	pa2m_afirmar(*anterior == NULL, "El elemento anterior es el correcto");
+	hash_insertar(hash, clave3, &elemento3, anterior);
+	hash_insertar(hash, clave4, &elemento4, anterior);
+	hash_insertar(hash, clave5, &elemento5, anterior);
+	hash_insertar(hash, clave6, &elemento6, anterior);
+	pa2m_afirmar(hash_cantidad(hash) == 6,
+		     "Se insertan varias elementos y causa rehash");
+
+	hash_destruir(hash);
+}
+
+void prueba_obtener_contener()
+{
+	int elemento1 = 1;
+	int elemento2 = 2;
+	int elemento3 = 3;
+	int elemento4 = 4;
+	int elemento5 = 5;
+	int elemento6 = 6;
+	void *elemento7 = NULL;
+	const char *clave_sin_insertar = "ABC";
+	void *elemento = &elemento1;
+	void **anterior = &elemento;
+
+	hash_t *hash = hash_crear(6);
+
+	hash_insertar(hash, clave1, &elemento1, anterior);
+	hash_insertar(hash, clave2, &elemento2, anterior);
+	hash_insertar(hash, clave3, &elemento3, anterior);
+	hash_insertar(hash, clave4, &elemento4, anterior);
+	hash_insertar(hash, clave5, &elemento5, anterior);
+	hash_insertar(hash, clave6, &elemento6, anterior);
+	hash_insertar(hash, clave7, elemento7, anterior);
+
+	pa2m_afirmar(!hash_contiene(hash, NULL),
+		     "Hash no contiene elementos con clave nula");
+	pa2m_afirmar(hash_obtener(hash, NULL) == NULL,
+		     "No se puede obtener un elemento con clave nula");
+	pa2m_afirmar(!hash_contiene(hash, clave_sin_insertar),
+		     "Hash no contiene una clave que no esta");
+	pa2m_afirmar(hash_obtener(hash, clave_sin_insertar) == NULL,
+		     "No se puede obtener una clave que no esta");
+	pa2m_afirmar(hash_contiene(hash, clave7),
+		     "Hash contiene elemento NULL");
+	pa2m_afirmar(hash_obtener(hash, clave7) == NULL,
+		     "Se obtiene elemento NULL");
+	hash_quitar(hash, clave7);
+	pa2m_afirmar(!hash_contiene(hash, clave7),
+		     "Se quita elemento y no lo contiene");
+	pa2m_afirmar(hash_obtener(hash, clave7) == NULL,
+		     "Se quita elemento y no se lo obtiene");
+	pa2m_nuevo_grupo("Pruebas de obtener y contener varios elementos");
+	pa2m_afirmar(hash_contiene(hash, clave1),
+		     "Hash contiene primer elemento");
+	pa2m_afirmar(hash_obtener(hash, clave1) == &elemento1,
+		     "Se obtiene primer elemento");
+	pa2m_afirmar(hash_contiene(hash, clave2),
+		     "Hash contiene segundo elemento");
+	pa2m_afirmar(hash_obtener(hash, clave2) == &elemento2,
+		     "Se obtiene segundo elemento");
+	pa2m_afirmar(hash_contiene(hash, clave3),
+		     "Hash contiene tercer elemento");
+	pa2m_afirmar(hash_obtener(hash, clave3) == &elemento3,
+		     "Se obtiene tercero elemento");
+	pa2m_afirmar(hash_contiene(hash, clave4),
+		     "Hash contiene cuarto elemento");
+	pa2m_afirmar(hash_obtener(hash, clave4) == &elemento4,
+		     "Se obtiene cuarto elemento");
+	pa2m_afirmar(hash_contiene(hash, clave5),
+		     "Hash contiene quinto elemento");
+	pa2m_afirmar(hash_obtener(hash, clave5) == &elemento5,
+		     "Se obtiene quinto elemento");
+	pa2m_afirmar(hash_contiene(hash, clave6),
+		     "Hash contiene sexto elemento");
+	pa2m_afirmar(hash_obtener(hash, clave6) == &elemento6,
+		     "Se obtiene sexto elemento");
+
+	hash_destruir(hash);
+}
+
+void prueba_eliminacion()
+{
+	int elemento1 = 1;
+	void *elemento = &elemento1;
+	void **anterior = &elemento;
+	hash_t *hash = hash_crear(0);
+	hash_insertar(hash, clave1, &elemento1, anterior);
+	hash_insertar(hash, clave2, NULL, anterior);
+	pa2m_afirmar(hash_quitar(hash, NULL) == NULL,
+		     "No se puede quitar una clave nula");
+	pa2m_afirmar(hash_quitar(hash, clave2) == NULL,
+		     "No se puede quitar un elemento que no existe");
+	pa2m_afirmar(hash_quitar(hash, clave2) == NULL,
+		     "Se quita un elemento NULL con clave valida");
+	pa2m_afirmar(hash_quitar(hash, clave1) == &elemento1,
+		     "Se quita un elemento con clave valida");
+	pa2m_afirmar(hash_quitar(hash, clave1) == NULL,
+		     "No se puede quitar el mismo elemento denuevo");
+	hash_destruir(hash);
+}
+
+void prueba_iterador_interno()
+{
+	int elemento1 = 1;
+	int elemento2 = 2;
+	int elemento3 = 3;
+	int elemento4 = 4;
+	int elemento5 = 5;
+	int elemento6 = 6;
+	void *elemento7 = NULL;
+	int elemento8 = 11;
+
+	void *elemento = &elemento1;
+	void **anterior = &elemento;
+	void *aux_nulo = NULL;
+	void *aux = &elemento8;
+	void *aux_corte = &elemento4;
+	hash_t *hash = hash_crear(6);
+
+	hash_insertar(hash, clave1, &elemento1, anterior);
+	hash_insertar(hash, clave2, &elemento2, anterior);
+	hash_insertar(hash, clave3, &elemento3, anterior);
+	hash_insertar(hash, clave4, &elemento4, anterior);
+	hash_insertar(hash, clave5, &elemento5, anterior);
+	hash_insertar(hash, clave6, &elemento6, anterior);
+	hash_insertar(hash, clave7, elemento7, anterior);
+	pa2m_afirmar(hash_con_cada_clave(hash, NULL, aux) == 0,
+		     "No se puede iterar con una funcion nula");
+	pa2m_afirmar(hash_con_cada_clave(hash, funcion_iteradora, aux_corte) !=
+			     7,
+		     "La iteracion se detiene correctamente");
+	pa2m_afirmar(hash_con_cada_clave(hash, funcion_iteradora, aux_nulo) ==
+			     7,
+		     "Se puede iterar con aux nulo");
+	pa2m_afirmar(hash_con_cada_clave(hash, funcion_iteradora, aux) == 7,
+		     "Se iteran todos los elementos");
+
+	hash_destruir_todo(hash, destructor);
 }
 
 int main()
 {
-	pa2m_nuevo_grupo(
-		"\n======================== XXX ========================");
-	prueba_simple();
-
+	pa2m_nuevo_grupo("Pruebas de creacion");
+	prueba_creacion();
+	pa2m_nuevo_grupo("Pruebas de hash vacio");
+	prueba_hash_vacio();
+	pa2m_nuevo_grupo("Pruebas de hash nulo");
+	prueba_hash_nulo();
+	pa2m_nuevo_grupo("Pruebas de inserccion");
+	prueba_inserccion();
+	pa2m_nuevo_grupo("Pruebas bordes de obtener y contener");
+	prueba_obtener_contener();
+	pa2m_nuevo_grupo("Pruebas de eliminacion");
+	prueba_eliminacion();
+	pa2m_nuevo_grupo("Pruebas de iterador interno");
+	prueba_iterador_interno();
 	return pa2m_mostrar_reporte();
 }
--- src/hash.c
+++ src/hash.c
@@ -1,72 +1,103 @@
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "hash.h"
 
 #define FACTOR_CARGA_MAXIMO 0.7
 const size_t CAPACIDAD_MINIMA = 3;
 
-struct par{
-	const char *clave;
+struct par {
+	char *clave;
 	void *valor;
 	struct par *siguiente;
 };
 
-struct hash{
+struct hash {
 	struct par **tabla;
 	size_t capacidad;
 	size_t cantidad;
 };
 
 hash_t *hash_crear(size_t capacidad)
-{	
-	hash_t *hash = calloc(1, sizeof(hash_t)); 
-	if(!hash)
+{
+	hash_t *hash = calloc(1, sizeof(hash_t));
+	if (!hash)
 		return NULL;
-	if(capacidad<=CAPACIDAD_MINIMA)
+	if (capacidad <= CAPACIDAD_MINIMA)
 		hash->capacidad = CAPACIDAD_MINIMA;
 	else
 		hash->capacidad = capacidad;
 
-	hash->tabla = calloc(hash->capacidad, sizeof(struct par*));
-	
-	if(!hash->tabla){
+	hash->tabla = calloc(hash->capacidad, sizeof(struct par *));
+	if (!hash->tabla) {
 		free(hash);
 		return NULL;
 	}
 
 	return hash;
-	
 }
 
-void rehashear(hash_t *hash_chico){
+unsigned long int funcion_hash(const char *clave)
+{
+	long unsigned int clave_hash = 0;
+	while (*clave) {
+		clave_hash = 31 * clave_hash + (long unsigned int)*clave;
+		clave++;
+	}
+	return clave_hash;
+}
+
+void hash_insertar_par(hash_t *hash, struct par *par)
+{
+	size_t posicion = funcion_hash(par->clave) % hash->capacidad;
+
+	if (!hash->tabla[posicion]) {
+		hash->tabla[posicion] = par;
+		return;
+	}
+
+	struct par *par_actual = hash->tabla[posicion];
+	while (par_actual) {
+		if (!par_actual->siguiente) {
+			par_actual->siguiente = par;
+			return;
+		}
+		par_actual = par_actual->siguiente;
+	}
+}
+
+void rehashear(hash_t *hash_chico)
+{
 	hash_t *hash_nuevo = hash_crear(hash_chico->capacidad * 2);
-	if(!hash_nuevo){
+
+	if (!hash_nuevo) {
 		hash_destruir(hash_chico);
 		return;
 	}
-		
-	for(int i=0; i<hash_chico->capacidad; i++){
-		struct par *actual = hash_chico->tabla[i];
-		while(actual){
-			void *anterior;
-			hash_insertar(hash_nuevo, actual->clave, actual->valor,
-		      &anterior);
-			actual = actual->siguiente;
+
+	for (int i = 0; i < hash_chico->capacidad; i++) {
+		struct par *par_actual = hash_chico->tabla[i];
+		while (par_actual) {
+			struct par *siguiente = par_actual->siguiente;
+			par_actual->siguiente = NULL;
+			hash_insertar_par(hash_nuevo, par_actual);
+			par_actual = siguiente;
 		}
 	}
 	free(hash_chico->tabla);
-	
-	
+	hash_chico->tabla = hash_nuevo->tabla;
+	hash_chico->capacidad = hash_nuevo->capacidad;
+	free(hash_nuevo);
 }
 
-int funcion_hash(const char *clave)
+char *crear_copia(const char *clave_original)
 {
-	int clave_hash = 0;
-	for(int i=0; i<strlen(clave);i++){
-		clave_hash += clave[i];
-	}
-	return clave_hash;
+	char *clave_copia = malloc(strlen(clave_original) + 1);
+	if (!clave_copia)
+		return NULL;
+	strcpy(clave_copia, clave_original);
+
+	return clave_copia;
 }
 
 hash_t *hash_insertar(hash_t *hash, const char *clave, void *elemento,
@@ -74,49 +105,76 @@ hash_t *hash_insertar(hash_t *hash, const char *clave, void *elemento,
 {
 	if (!hash || !clave)
 		return NULL;
-	
-	float factor_carga = (float)hash->cantidad / (float)hash->capacidad ;
-	if(factor_carga > FACTOR_CARGA_MAXIMO){
+
+	float factor_carga = (float)hash->cantidad / (float)hash->capacidad;
+	if (factor_carga > FACTOR_CARGA_MAXIMO) {
 		rehashear(hash);
 	}
 
-	int posicion = funcion_hash(clave) % (int)hash->capacidad;
-	struct par *actual = hash->tabla[posicion];
-	while(actual){
-		if(actual->clave == clave){
-			if(anterior)
-				*anterior = actual->valor;
-			actual->valor = elemento;
+	size_t posicion = funcion_hash(clave) % hash->capacidad;
+
+	if (!hash->tabla[posicion]) {
+		struct par *nuevo_par = calloc(1, sizeof(struct par));
+		nuevo_par->clave = crear_copia(clave);
+		if (!nuevo_par->clave) {
+			free(nuevo_par);
+			return NULL;
+		}
+		nuevo_par->valor = elemento;
+		hash->tabla[posicion] = nuevo_par;
+		hash->cantidad++;
+		if (anterior)
+			*anterior = NULL;
+		return hash;
+	}
+
+	struct par *par_actual = hash->tabla[posicion];
+	while (par_actual) {
+		if (strcmp(par_actual->clave, clave) == 0) {
+			if (anterior)
+				*anterior = par_actual->valor;
+			par_actual->valor = elemento;
 			return hash;
 		}
-		actual = actual->siguiente;
+		par_actual = par_actual->siguiente;
 	}
 
-	struct par *nuevo_par = malloc(sizeof(struct par));
-	if(!nuevo_par)
+	struct par *nuevo_par = calloc(1, sizeof(struct par));
+	if (!nuevo_par)
 		return NULL;
-	nuevo_par->clave = clave;
+	nuevo_par->clave = crear_copia(clave);
+	if (!nuevo_par->clave) {
+		free(nuevo_par);
+		return NULL;
+	}
 	nuevo_par->valor = elemento;
-	nuevo_par->siguiente = hash->tabla[posicion];
-	hash->tabla[posicion] = nuevo_par;
-	hash->cantidad++;
-	if(anterior)
-		*anterior = NULL;
-	return hash;
+	struct par *par_corriente = hash->tabla[posicion];
+	while (par_corriente) {
+		if (!par_corriente->siguiente) {
+			par_corriente->siguiente = nuevo_par;
+			hash->cantidad++;
+			if (anterior)
+				*anterior = NULL;
+			return hash;
+		}
+		par_corriente = par_corriente->siguiente;
+	}
+
+	return NULL;
 }
 
-struct par *buscar_par_anterior(struct par *actual, const char *clave)
+struct par *buscar_par_anterior(struct par *par_actual, const char *clave_copia)
 {
-	if(!actual)
+	if (!par_actual)
 		return NULL;
 
-	if(actual->siguiente){
-		if(actual->siguiente->clave == clave)
-			return actual;
+	while (par_actual->siguiente) {
+		if (strcmp(par_actual->siguiente->clave, clave_copia) == 0)
+			return par_actual;
+		par_actual = par_actual->siguiente;
 	}
-	
 
-	return buscar_par_anterior(actual->siguiente, clave);
+	return NULL;
 }
 
 void *hash_quitar(hash_t *hash, const char *clave)
@@ -124,27 +182,45 @@ void *hash_quitar(hash_t *hash, const char *clave)
 	if (!hash || !clave)
 		return NULL;
 
-	int posicion = funcion_hash(clave) % (int)hash->capacidad;
-	struct par *actual = hash->tabla[posicion];
-	if(actual && actual->clave == clave){
-		void *elemento_eliminado = actual->valor;
+	size_t posicion = funcion_hash(clave) % hash->capacidad;
+	if (!hash->tabla[posicion])
+		return NULL;
+	struct par *par_actual = hash->tabla[posicion];
+	if (par_actual && strcmp(par_actual->clave, clave) == 0) {
+		void *elemento_eliminado = par_actual->valor;
 		hash->tabla[posicion] = hash->tabla[posicion]->siguiente;
-		free(actual);
+		free(par_actual->clave);
+		free(par_actual);
 		hash->cantidad--;
 		return elemento_eliminado;
 	}
-	
-	struct par *par_anterior = buscar_par_anterior(actual, clave);
-	if(!par_anterior)
+
+	struct par *par_anterior = buscar_par_anterior(par_actual, clave);
+	if (!par_anterior)
 		return NULL;
 	struct par *par_auxiliar = par_anterior->siguiente;
 	void *elemento_eliminado = par_auxiliar->valor;
-	par_anterior->siguiente =  par_auxiliar->siguiente;
-
+	par_anterior->siguiente = par_auxiliar->siguiente;
+	free(par_auxiliar->clave);
 	free(par_auxiliar);
 	hash->cantidad--;
 
 	return elemento_eliminado;
+}
+
+struct par *obtener_par(hash_t *hash, const char *clave)
+{
+	size_t posicion = funcion_hash(clave) % hash->capacidad;
+	if (!hash->tabla[posicion])
+		return NULL;
+	struct par *par_actual = hash->tabla[posicion];
+	while (par_actual) {
+		if (strcmp(par_actual->clave, clave) == 0) {
+			return par_actual;
+		}
+		par_actual = par_actual->siguiente;
+	}
+	return NULL;
 }
 
 void *hash_obtener(hash_t *hash, const char *clave)
@@ -152,14 +228,9 @@ void *hash_obtener(hash_t *hash, const char *clave)
 	if (!hash || !clave)
 		return NULL;
 
-	int posicion = funcion_hash(clave) % (int)hash->capacidad;
-	struct par *actual = hash->tabla[posicion];
-	while(actual){
-		if(actual->clave == clave){
-			return actual->valor;
-		}
-		actual = actual->siguiente;
-	}
+	struct par *par_clave = obtener_par(hash, clave);
+	if (par_clave)
+		return par_clave->valor;
 	return NULL;
 }
 
@@ -167,9 +238,10 @@ bool hash_contiene(hash_t *hash, const char *clave)
 {
 	if (!hash || !clave)
 		return false;
-	if(!hash_obtener(hash, clave))
-		return false;
-	return true;
+	struct par *par_clave = obtener_par(hash, clave);
+	if (par_clave)
+		return true;
+	return false;
 }
 
 size_t hash_cantidad(hash_t *hash)
@@ -181,14 +253,15 @@ size_t hash_cantidad(hash_t *hash)
 
 void hash_destruir(hash_t *hash)
 {
-	if(!hash)
+	if (!hash)
 		return;
-	for(int i=0; i<hash->capacidad; i++){
-		struct par *actual = hash->tabla[i];
-		while(actual){
-			struct par *auxiliar = actual;
-			actual = actual->siguiente;
-			free(auxiliar);
+	for (int i = 0; i < hash->capacidad; i++) {
+		struct par *par_actual = hash->tabla[i];
+		while (par_actual) {
+			struct par *siguiente = par_actual->siguiente;
+			free(par_actual->clave);
+			free(par_actual);
+			par_actual = siguiente;
 		}
 	}
 	free(hash->tabla);
@@ -197,21 +270,25 @@ void hash_destruir(hash_t *hash)
 
 void hash_destruir_todo(hash_t *hash, void (*destructor)(void *))
 {
-	if (!hash || !destructor)
+	if (!hash)
 		return;
+	if (!destructor) {
+		hash_destruir(hash);
+		return;
+	}
 
-	for(int i=0; i<hash->capacidad; i++){
-		struct par *actual = hash->tabla[i];
-		while(actual){
-			struct par *auxiliar = actual;
-			actual = actual->siguiente;
-			destructor(auxiliar->valor);
-			free(auxiliar);
+	for (int i = 0; i < hash->capacidad; i++) {
+		struct par *par_actual = hash->tabla[i];
+		while (par_actual) {
+			struct par *siguiente = par_actual->siguiente;
+			destructor(par_actual->valor);
+			free(par_actual->clave);
+			free(par_actual);
+			par_actual = siguiente;
 		}
 	}
 	free(hash->tabla);
 	free(hash);
-
 }
 
 size_t hash_con_cada_clave(hash_t *hash,
@@ -222,12 +299,12 @@ size_t hash_con_cada_clave(hash_t *hash,
 	if (!hash || !f)
 		return iterados;
 	bool seguir = true;
-	for(int i=0;i<hash->capacidad && seguir;i++){
-		struct par *actual = hash->tabla[i];
-		while(actual && seguir){
-			seguir = f(actual->clave, actual->valor, aux);
-			actual = actual->siguiente;
+	for (int i = 0; i < hash->capacidad && seguir; i++) {
+		struct par *par_actual = hash->tabla[i];
+		while (par_actual && seguir) {
+			seguir = f(par_actual->clave, par_actual->valor, aux);
 			iterados++;
+			par_actual = par_actual->siguiente;
 		}
 	}
 	return iterados;
